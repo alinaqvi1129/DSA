384bb8bec9f0ea03316348d7ffbdc23b043a9e79
Ali Naqvi <shakeelnaqvi35@gmail.com>
2026-09-22 21:04:26 +0530

    Time: 1 ms (24.94%) | Memory: 50.1 MB (66.91%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int max = 0;
+        for(int i = 0;i<citations.length;i++){
+            int papers = citations.length - i;
+            if(citations[i] >= papers){
+                max = Math.max(max,papers);
+            }
+        }
+        return max;
+    }
+}
